d87b8a5e9f061c0b43d968cc90daf220a719c7df
alexanderPat0 <alex0rodriguez7fenix@gmail.com>
2023-11-21 10:15:00 +0100

    Creadas clases para las tarjetas

---
+++ lib/screens/user/cards/emotion_card.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class EmotionCard extends StatelessWidget {
+  const EmotionCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text('Hola');
+  }
+}
---
+++ lib/screens/user/cards/mood_card.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+class MoodCard extends StatelessWidget {
+  const MoodCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text('Hola');
+  }
+}
