4847acb48d4c1d0239d2ea6473cc761cfe538091
SamuraiJuanjo <Juanjosepi.orellana19@gmail.com>
2023-11-21 10:20:36 +0100

    Added button of cards

--- lib/screens/user/main_screen.dart
+++ lib/screens/user/main_screen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:mindcare_app/screens/user/cards/emotion_card.dart';
+import 'package:mindcare_app/screens/user/cards/event_card.dart';
+import 'package:mindcare_app/screens/user/cards/mood_card.dart';
 
 class _TabInfo {
   const _TabInfo(this.title, this.icon);
@@ -14,19 +18,15 @@ class MainScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final tabInfo = [
       const _TabInfo(
-        ('Diary'),
+        'Diary',
         CupertinoIcons.book_circle,
       ),
-      // const _TabInfo(
-      //   ('conversation'),
-      //   CupertinoIcons.conversation_bubble,
-      // ),
       const _TabInfo(
-        ('Profile'),
+        'Profile',
         CupertinoIcons.profile_circled,
       ),
       const _TabInfo(
-        ('Settings'),
+        'Settings',
         CupertinoIcons.settings,
       ),
     ];
@@ -73,15 +73,143 @@ class _CupertinoDemoTab extends StatelessWidget {
     return CupertinoPageScaffold(
       navigationBar: const CupertinoNavigationBar(),
       backgroundColor: CupertinoColors.systemBackground,
-      child: Center(
-        child: Icon(
-          icon,
-          semanticLabel: title,
-          size: 100,
-        ),
+      child: Stack(
+        children: [
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(
+                  icon,
+                  semanticLabel: title,
+                  size: 100,
+                ),
+              ],
+            ),
+          ),
+          Positioned(
+            bottom: 70.0,
+            right: 20.0,
+            child: _FloatingActionButtonGroup(),
+          ),
+        ],
       ),
     );
   }
 }
 
+class _FloatingActionButtonGroup extends StatefulWidget {
+  @override
+  _FloatingActionButtonGroupState createState() =>
+      _FloatingActionButtonGroupState();
+}
 
+class _FloatingActionButtonGroupState
+    extends State<_FloatingActionButtonGroup> {
+  bool _isExpanded = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: [
+        if (_isExpanded)
+          _CustomFloatingActionButton(
+            onPressed: () {
+              Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => const EmotionCard()),
+          );
+            },
+            tooltip: 'Emotions',
+            heroTag: null,
+            icon: const Icon(Icons.lightbulb),
+            label: 'Emotions',
+          ),
+        if (_isExpanded)
+          const SizedBox(
+            height: 16,
+          ),
+        if (_isExpanded)
+          _CustomFloatingActionButton(
+            onPressed: () {
+              Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => const MoodCard()),
+          );
+            },
+            tooltip: 'Mood',
+            heroTag: null,
+            icon: const Icon(Icons.emoji_emotions),
+            label: 'Mood',
+          ),
+        if (_isExpanded)
+          const SizedBox(
+            height: 16,
+          ),
+        if (_isExpanded)
+          _CustomFloatingActionButton(
+            onPressed: () {
+              Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => const EventCard()),
+          );
+            },
+            tooltip: 'Events',
+            heroTag: null,
+            icon: const Icon(Icons.star),
+            label: 'Events',
+          ),
+        const SizedBox(
+          height: 16,
+        ),
+        FloatingActionButton(
+          onPressed: () {
+            setState(() {
+              _isExpanded = !_isExpanded;
+            });
+          },
+          tooltip: 'Toggle buttons',
+          heroTag: null,
+          child: _isExpanded ? const Icon(Icons.close) : const Icon(Icons.add),
+        ),
+      ],
+    );
+  }
+}
+
+class _CustomFloatingActionButton extends StatelessWidget {
+  const _CustomFloatingActionButton({
+    Key? key,
+    required this.onPressed,
+    required this.tooltip,
+    required this.heroTag,
+    required this.icon,
+    required this.label,
+  }) : super(key: key);
+
+  final VoidCallback onPressed;
+  final String tooltip;
+  final Object? heroTag;
+  final Icon icon;
+  final String label;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        FloatingActionButton(
+          onPressed: onPressed,
+          tooltip: tooltip,
+          heroTag: heroTag,
+          child: icon,
+        ),
+        const SizedBox(height: 1),
+        Text(
+        label,
+        style: const TextStyle(fontSize: 12), // Ajusta el tamaño del texto según tus preferencias
+      ),
+      ],
+    );
+  }
+}
