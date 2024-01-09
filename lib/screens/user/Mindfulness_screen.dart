9d4376721a0938ced7786a054e05cfee29fa32ad
SamuraiJuanjo <Juanjosepi.orellana19@gmail.com>
2024-01-09 10:47:33 +0100

    changed imgs in card_swiper

--- lib/screens/user/Mindfulness_screen.dart
+++ lib/screens/user/Mindfulness_screen.dart
@@ -9,7 +9,7 @@ class MindFulnessScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final cardWidth = MediaQuery.of(context).size.width * 0.8;
-    final cardHeight = (cardWidth * 0.8);
+    final cardHeight = (cardWidth * 0.7);
 
     return CupertinoPageScaffold(
       navigationBar: const CupertinoNavigationBar(),
@@ -19,22 +19,27 @@ class MindFulnessScreen extends StatelessWidget {
         ),
         child: ListView(
           children: [
-            buildSwiper(cardWidth, cardHeight),
-            buildSwiper(cardWidth, cardHeight),
-            buildSwiper(cardWidth, cardHeight),
+            Text("Meditation",),
+            buildSwiperMeditation(cardWidth, cardHeight),
+            Text("Relaxation",),
+            buildSwiperRelaxation(cardWidth, cardHeight),
+            Text("Breathing",),
+            buildSwiperBreathing(cardWidth, cardHeight),
           ],
         ),
       ),
     );
   }
 
-  Widget buildSwiper(double cardWidth, double cardHeight) {
+  Widget buildSwiperMeditation(double cardWidth, double cardHeight) {
     return Container(
       width: cardWidth,
       height: cardHeight,
       margin: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 8.0),
       child: Swiper(
         itemCount: 3,
+        pagination: const SwiperPagination(),
+        //control: const SwiperControl(),
         itemBuilder: (BuildContext context, int index) {
           return Card(
             elevation: 4.0,
@@ -45,7 +50,95 @@ class MindFulnessScreen extends StatelessWidget {
               width: cardWidth,
               decoration: BoxDecoration(
                 image: const DecorationImage(
-                  image: AssetImage('assets/screen_images/meditation.jpg'),
+                  image: AssetImage('assets/screen_images/meditacion.png'),
+                  fit: BoxFit.scaleDown,
+                ),
+                borderRadius: BorderRadius.circular(16.0),
+              ),
+              // Puedes personalizar el contenido de cada tarjeta aquí
+              // Por ejemplo, agregar texto o botones
+              child: Center(
+                child: Text(
+                  'Tarjeta ${index + 1}',
+                  style: const TextStyle(
+                    fontSize: 24.0,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+          );
+        },
+        autoplay: true,
+      ),
+    );
+  }
+
+  Widget buildSwiperRelaxation(double cardWidth, double cardHeight) {
+    return Container(
+      width: cardWidth,
+      height: cardHeight,
+      margin: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 8.0),
+      child: Swiper(
+        itemCount: 3,
+        pagination: const SwiperPagination(),
+        //control: const SwiperControl(),
+        itemBuilder: (BuildContext context, int index) {
+          return Card(
+            elevation: 4.0,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(16.0),
+            ),
+            child: Container(
+              width: cardWidth,
+              decoration: BoxDecoration(
+                image: const DecorationImage(
+                  image: AssetImage('assets/screen_images/relaxation.png'),
+                  fit: BoxFit.scaleDown,
+                ),
+                borderRadius: BorderRadius.circular(16.0),
+              ),
+              // Puedes personalizar el contenido de cada tarjeta aquí
+              // Por ejemplo, agregar texto o botones
+              child: Center(
+                child: Text(
+                  'Tarjeta ${index + 1}',
+                  style: const TextStyle(
+                    fontSize: 24.0,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+          );
+        },
+        autoplay: true,
+      ),
+    );
+  }
+
+  Widget buildSwiperBreathing(double cardWidth, double cardHeight) {
+    return Container(
+      width: cardWidth,
+      height: cardHeight,
+      margin: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 8.0),
+      child: Swiper(
+        itemCount: 3,
+        pagination: const SwiperPagination(),
+        //control: const SwiperControl(),
+        itemBuilder: (BuildContext context, int index) {
+          return Card(
+            elevation: 4.0,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(16.0),
+            ),
+            child: Container(
+              width: cardWidth,
+              decoration: BoxDecoration(
+                image: const DecorationImage(
+                  image: AssetImage('assets/screen_images/breathing.png'),
                   fit: BoxFit.scaleDown,
                 ),
                 borderRadius: BorderRadius.circular(16.0),
@@ -70,3 +163,5 @@ class MindFulnessScreen extends StatelessWidget {
     );
   }
 }
+
+
